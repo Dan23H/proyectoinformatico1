7ceec0ee629fd69fee58cecef4bcc34b4fe14ea2
david-ale <125339205+david-ale@users.noreply.github.com>
2024-11-11 18:31:55 -0500

    0.0.5 FEAT: añado ultimo query y controlador. ya estan testeados

--- db/Controllers/ultrasoundController.js
+++ db/Controllers/ultrasoundController.js
@@ -1,5 +1,6 @@
 const newUltrasound = require ('../Functions/createUltrasound');
 const viewHistory = require('../Functions/getHistory');
+const viewUltrasound = require('../Functions/viewUltrasound');
 const Patient = require('../models/patient')
 
 const createUltrasound = async (req, res) => {
@@ -27,4 +28,18 @@ const ultrasoundHistory = async (req, res) => {
     }
 }
 
-module.exports = {createUltrasound, ultrasoundHistory};
+const ultrasoundInfo = async(req,res) => {
+    const {ultrasoundId} = req.params;
+    try {
+        const ultrasound = await viewUltrasound(ultrasoundId);
+        if (ultrasound) {
+            return res.status(200).json({ success: true, data: ultrasound });
+        } else {
+            return res.status(404).json({ success: false, message: 'Ecografía no encontrada' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {createUltrasound, ultrasoundHistory, ultrasoundInfo};
--- db/routes/ultrasoundRoutes.js
+++ db/routes/ultrasoundRoutes.js
@@ -8,4 +8,7 @@ router.post('/create-ultrasound', ultrasoundController.createUltrasound);
 //ver historial de ecografias
 router.get('/ultrasound-history/:patientId', ultrasoundController.ultrasoundHistory)
 
+//ver ecografia de un paciente
+router.get('/ultrasound/:ultrasoundId', ultrasoundController.ultrasoundInfo)
+
 module.exports = router;
