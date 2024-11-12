1d2186ded8c1533894f8d858d6dfb55f0f67f02f
SimonR2003 <simon.ruizcam@gmail.com>
2024-11-12 01:04:58 -0500

    0.0.2 FEAT: Login y Historial

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import videoController from '../controllers/videoController';
+import loginController from '../controllers/loginController';
+import { getUltrasoundHistory } from '../controllers/historialPaciente';
 
 const router = express.Router();
 
@@ -7,6 +9,30 @@ const router = express.Router();
 router.post('/convert',
      videoController.uploadMiddleware,
      videoController.convertVideo
+);
+
+router.post('/login', 
+     loginController     
+);
+
+router.get('/historial-paciente',
+     getUltrasoundHistory
+);
+
+router.get('/obtener-pacientes',
+
+);
+
+router.post('/crear-paciente', 
+
+);
+
+router.get('/obtener-paciente',
+
+);
+
+router.get('/crear-medico',
+
 );
 
 export default router;
