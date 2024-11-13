346a0f978acb494decb80cc9a2aaaa6626468712
HexDan <123597766+Dan23H@users.noreply.github.com>
2024-11-13 04:29:43 -0500

    0.0.9 FEAT: Nueva página principal para pacientes

--- front/src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-    return (
-      <div>
-        <h1>Bienvenido a tu página principal</h1>
-        <p>Has iniciado sesión correctamente.</p>
-      </div>
-    );
-  }
-  
